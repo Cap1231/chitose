ccddcbfddd87c601c92817d1c833fac985f2cd72
tsubasa <tsubasa.y1231@gmail.com>
2020-08-17 12:28:59 +0900

    相対値に変更

--- triangle.cpp
+++ triangle.cpp
@@ -3,32 +3,35 @@
 #include <cmath>
 using namespace std;
 
-template
-<
-  typename TYPE,
-  std::size_t SIZE
->
+template <
+    typename TYPE,
+    std::size_t SIZE>
 std::size_t array_length(const TYPE (&)[SIZE])
-{   
+{
   return SIZE;
 }
 
-struct Coordinate {
+// 三角形情報を保持する構造体
+struct Coordinate
+{
   double x0;
   double y0;
   double x1;
   double y1;
   double x2;
   double y2;
-  double gradient1 () {
+  double gradient1()
+  {
     double g1 = (y1 - y0) / (x1 - x0);
-    return fabs(g1);
+    return g1;
   }
-  double gradient2 () {
+  double gradient2()
+  {
     double g2 = (y2 - y1) / (x2 - x1);
-    return fabs(g2);
+    return g2;
   }
-  void print() {
+  void print()
+  {
     cout << fixed << setprecision(1) << endl;
     cout << "[x0, y0] = [" << x0 << ", " << y0 << "]" << endl;
     cout << "[x1, y1] = [" << x1 << ", " << y1 << "]" << endl;
@@ -36,44 +39,58 @@ struct Coordinate {
   }
 };
 
-bool is_triangle(Coordinate* pcoordicate) {
+bool is_triangle(Coordinate *pcoordicate)
+{
   Coordinate coordinate = *pcoordicate;
 
   // 座標から２点を選び、傾き(絶対値)を取得
   // 傾きが異なるなら、三角形（直線状にない）
   // 傾きが同じなら、直線（直線上にある)
-  if (coordinate.gradient1() != coordinate.gradient2()) {
+  if (coordinate.gradient1() != coordinate.gradient2())
+  {
     return true;
-  } else {
+  }
+  else
+  {
     return false;
   }
 }
 
-int main(void) {
+int main(void)
+{
   // サンプルデータ
-  double clist[][6] =  {
-    {1.0, 4.0, 2.0, 5.0, 3.0, 3.0},
-    {1.0, 1.0, 3.0, 3.0, 5.0, 5.0},
-    {2.0, 3.0, 3.0, 5.0, 6.0, 8.0},
-    {2.0, 3.0, 3.0, 5.0, 6.0, 11.0},
-    {3.0, 4.7, 2.7, 5.9, 3.4, 3.46666},
-    {4.0, 9.8, 7.3, 6.7, 3.7, 7.8},
+  double clist[][6] = {
+      {1.0, 4.0, 2.0, 5.0, 3.0, 3.0},
+      {1.0, 1.0, 3.0, 3.0, 5.0, 5.0},
+      {2.0, 3.0, 3.0, 5.0, 6.0, 8.0},
+      {2.0, 3.0, 3.0, 5.0, 6.0, 11.0},
+      {3.0, 4.7, 2.7, 5.9, 3.4, 3.46666},
+      {4.0, 9.8, 7.3, 6.7, 3.7, 7.8},
   };
 
-  for (int i = 0; i < array_length(clist); i++ ) {
+  for (int i = 0; i < array_length(clist); i++)
+  {
     Coordinate coordinate = {
-      clist[i][0],
-      clist[i][1],
-      clist[i][2],
-      clist[i][3],
-      clist[i][4],
-      clist[i][5]
-    };
+        clist[i][0],
+        clist[i][1],
+        clist[i][2],
+        clist[i][3],
+        clist[i][4],
+        clist[i][5]};
+
+    // 三角形座標表示
     coordinate.print();
-    if (is_triangle(&coordinate) == true) {
-      cout << "三角形です" << endl << endl;
-    } else {
-      cout << "三角形ではないです" << endl << endl;
+
+    // 三角形判定
+    if (is_triangle(&coordinate) == true)
+    {
+      cout << "三角形です" << endl
+           << endl;
+    }
+    else
+    {
+      cout << "三角形ではないです" << endl
+           << endl;
     }
   };
 
